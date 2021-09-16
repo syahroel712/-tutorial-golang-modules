d89d2ac59a91d4bc173988157f2b32dd96bf8e8e
syahroel712 <syahroel712@gmail.com>
2021-09-16 11:35:08 +0700

    Pengenalan Go Modules

---
+++ 1-go-modules.go
@@ -0,0 +1,16 @@
+
+
+
+
+
+// catatan
+// sebelum ada go modules
+	// saat buat aplikasi , biasanya kita akan menggunakan library / depedency dari project lain
+	// sebelum ada go modules, manajement utk dependency sangat sulit dilakukan di go
+	// biasanya kita akan mengkopy semua source code library / dependency lain ke project kita, hal ini membuat project kita menjadi bengkak karena penuh dengan library orang lain
+	// atau bianay library orang akan kita save di GOPATH directory, namun hal ini akan sulit jika ternyata beberapa aplikasi butuh library yg sama dengan versi yg berbeda
+
+// sekarang sudah ada go modules 
+// mulai dikenal di go 1.11 and 1.12
+// degan ini, kita mudah sangat dipenmudah
+// ini mirip npm , composer
