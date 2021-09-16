41aa031712a87cd3db0c037a9788df3c50c6f318
syahroel712 <syahroel712@gmail.com>
2021-09-16 22:32:50 +0700

    Major Upgrade

---
+++ 6-major-upgrade.go
@@ -0,0 +1,8 @@
+
+
+
+// catatan
+// major upgrade
+// terjadi karena ada perubahan pada isi kode program, sehingga membuat tidak backword compatible
+// sebaiknya hal ini dihindari
+// namun jika tidak bisa dihindari, strategi terbaik adalah merubah nama module
