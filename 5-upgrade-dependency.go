50cb681bc63365b2e1d711491cce4b3790ae6dd3
syahroel712 <syahroel712@gmail.com>
2021-09-16 22:12:11 +0700

    Upgrade Dependency

---
+++ 5-upgrade-dependency.go
@@ -0,0 +1,9 @@
+
+
+
+
+// catatan
+// upgrade dependency
+// untuk update dependency ke versi terbaru, kita bisa mengubah isi go.mod, lalu mengubah tagnya menjadi tag terbaru
+// untuk mendownload versi terbaru gunakan perintah go get
+// disarankan agar ketika merubah sedikit dependency pakai v.1.0.1 agar go bisa mendownload, karena go akan mencache data yg sudah pernah di download
