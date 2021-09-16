3fd91015f16da7bcea5791d59a7312855767754e
syahroel712 <syahroel712@gmail.com>
2021-09-16 22:00:12 +0700

    Membuat module

---
+++ 2-membuat-module.go
@@ -0,0 +1,16 @@
+
+
+
+
+
+// catatan
+// membuat module
+// utk membuat module baru, kita bisa menggunakan porintah : go mod init nama-module
+// golang akan otomatis membuat file go.mod yg berisi info nama module dan juga versi go yg digunakan
+
+// rilis module
+// go terintegrasi dengan baik dengan git
+// untuk merilis module, kita hanya perlu membuat tag di git
+// biasa kan gunakan v saat rilis tag
+	// contoh : v1.0.0
+	// 			v1.0.1
